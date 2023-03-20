a4ca63409895b67e4672c4f2e2121d905ad26c9c
Abel Philip <josephapj007@gmail.com>
2023-03-21 00:13:15 +0530

    find us add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,16 +57,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-  var selectedIndex =0;
-
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-
     Widget page;
-    switch (selectedIndex){
+    switch (selectedIndex) {
       case 0:
         page = Generator();
         break;
@@ -73,72 +72,79 @@ class _MyHomePageState extends State<MyHomePage> {
       case 2:
         page = Placeholder();
         break;
+      case 3:
+        page = FindUs();
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
-    return LayoutBuilder(
-      builder: (context,constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text(
-                        'Home',
-                        style: TextStyle(
-                          fontFamily: 'MontserratAlternates',
-                          fontSize: 22,
-                        ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text(
+                      'Home',
+                      style: TextStyle(
+                        fontFamily: 'MontserratAlternates',
+                        fontSize: 22,
                       ),
                     ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text(
-                        'Favorites',
-                        style: TextStyle(
-                          fontFamily: 'MontserratAlternates',
-                          fontSize: 22,
-                        ),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text(
+                      'Favorites',
+                      style: TextStyle(
+                        fontFamily: 'MontserratAlternates',
+                        fontSize: 22,
                       ),
                     ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.my_library_add), 
-                      label:  Text(
-                        'Library',
-                        style: TextStyle(
-                          fontFamily: 'MontserratAlternates',
-                          fontSize: 22,
-                        ),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.my_library_add),
+                    label: Text(
+                      'Library',
+                      style: TextStyle(
+                        fontFamily: 'MontserratAlternates',
+                        fontSize: 22,
                       ),
                     ),
-              
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                   
-                      setState(() {
-                        selectedIndex=value;
-                      });
-
-
-                  },
-                ),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.map),
+                    label: Text(
+                      'Find Us',
+                      style: TextStyle(
+                        fontFamily: 'MontserratAlternates',
+                        fontSize: 22,
+                      ),
+                    ),
+                  )
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
               ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
               ),
-            ],
-          ),
-        );
-      }
-    );
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
 
@@ -157,45 +163,44 @@ class Generator extends StatelessWidget {
     }
 
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text('A random amazing idea: '),
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            //button
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFav();
-                  },
-                  icon: Icon(icon),
-                  label: Text(
-                    'like',
-                    style: TextStyle(
-                      fontFamily: 'MontserratAlternates',
-                      fontSize: 22,
-                    ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Text('A random amazing idea: '),
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          //button
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFav();
+                },
+                icon: Icon(icon),
+                label: Text(
+                  'like',
+                  style: TextStyle(
+                    fontFamily: 'MontserratAlternates',
+                    fontSize: 22,
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text(
-                    'next',
-                    style: TextStyle(
-                        fontFamily: 'MontserratAlternates', fontSize: 22),
-                  ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text(
+                  'next',
+                  style: TextStyle(
+                      fontFamily: 'MontserratAlternates', fontSize: 22),
                 ),
-              ],
-            ),
-          ],
-        ),
-      );
-    
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -228,39 +233,98 @@ class BigCard extends StatelessWidget {
   }
 }
 
-class FavouritesPage extends StatelessWidget{
+class FavouritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-     var appState = context.watch<MyAppState>();
-      var pair = appState.current;
-     if(appState.fav.isEmpty){
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+    if (appState.fav.isEmpty) {
       return Center(
         child: Text('No favourites yet!'),
       );
-     }
-
-
+    }
 
     return ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text('You have ${appState.fav.length} favourites'),
-
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ${appState.fav.length} favourites'),
+        ),
+        for (var pair in appState.fav)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
           ),
-          for ( var pair in appState.fav)
-            ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text(pair.asLowerCase),
-              ),
-          
-        ],
-      
-       
-      
+      ],
     );
-    
   }
-  
+}
+
+// ignore: must_be_immutable
+class FindUs extends StatelessWidget {
+  late GoogleMapController mapController;
+  List <Marker>_markers=[];
+  final List<Marker> marker = const [
+    Marker(
+        markerId: MarkerId("Kochi"),
+        position: LatLng(9.9312, 76.2673),
+        infoWindow: InfoWindow(
+          title: "Kochi",
+        )),
+    Marker(
+        markerId: MarkerId("Trivandrum"),
+        position: LatLng(8.5241, 76.9366),
+        infoWindow: InfoWindow(
+          title: "Trivandrum",
+        )),
+    Marker(
+        markerId: MarkerId("Goa"),
+        position: LatLng(15.2993, 74.1240),
+        infoWindow: InfoWindow(
+          title: "Goa",
+        )),
+    Marker(
+        markerId: MarkerId("Alappuzha"),
+        position: LatLng(9.4981, 76.3388),
+        infoWindow: InfoWindow(
+          title: "Alappuzha",
+        ))
+  ];
+
+   void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    _markers.addAll(marker);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+          title: const Text(
+            'Our Offices',
+            style: TextStyle(
+              fontFamily: 'MontserratAlternates',
+              fontSize: 22,
+            ),
+          ),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(9.9312, 76.2673),
+            zoom: 7,
+          ),
+          markers: Set.of(marker) ,
+        ),
+      ),
+    );
+  }
 }
